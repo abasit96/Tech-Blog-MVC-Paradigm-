1c21466fdddfb6eb838507b3d5cfb8272c4303b0
Bootcamp <bootcamp@Abduls-Air.hsd1.va.comcast.net>
2023-05-19 00:38:04 -0700

    Initial commit

--- Develop/controllers/api/postRoutes.js
+++ Develop/controllers/api/postRoutes.js
@@ -15,6 +15,26 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  const existingPost = await Post.update(
+    {
+      ...req.body,
+      user_id: req.session.user_id,
+    },
+    {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    }
+  );
+
+  res.status(200).json(existingPost);
+} catch (err) {
+  res.status(400).json(err);
+}
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
